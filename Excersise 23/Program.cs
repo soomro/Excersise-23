bb8584d010eb9905622dfce89887b468ae40cd8b
Waseem Akhtar Soomro <ws@waseemsoomro.com>
2016-02-04 11:04:14 +0100

    excersize 23 - assignment 1

---
+++ Excersise 23/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excersise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                val = i;
+                for (int j = 1; j < 11; j++)
+                {
+                    Console.Write("{0}\t ", val);
+                    val += i;
+                }
+
+                Console.Write("\n");
+            }
+            
+            Console.ReadLine();
+        }
+       
+    }
+}
